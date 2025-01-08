4873c94c5dadcc537964bc4295520e4218cb208c
Kiikoolian <bowser2201@gmail.com>
2025-01-08 21:22:02 +0100

    JavaScript variables and operators finished

---
+++ Foundations Course/Javascript/Variables-and-operators/javascript.js
@@ -0,0 +1,46 @@
+// let name = "John";
+// let surname = "Doe";
+
+// console.log(name);
+// console.log(surname);
+
+// let age = 11;
+// console.log(age); // outputs 11 to the console
+
+// age = 54;
+ 
+// console.log(age); // what will be output now?
+
+
+// const pi = 3.14;
+//  pi = 10;
+
+// console.log(pi); // What will be output? - An error
+
+// console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23 + 97);
+
+console.log(23 + 97+(3 + 2) - 76 * (1 + 1));
+
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a,"a");
+
+console.log(9 * a,"a");
+
+let b = 7 * a;
+
+console.log(b,"b");
+
+
+let max = 57;
+
+let actual = max-13;
+
+let percentage = actual/max;
+
+console.log(percentage,"percentage");
