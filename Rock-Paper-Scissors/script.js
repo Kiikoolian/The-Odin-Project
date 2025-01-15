d1d4f4cffb8a18b43a537845e07ebc482ddc621e
Kiikoolian <bowser2201@gmail.com>
2025-01-15 13:34:35 +0100

    Rock Paper Scissors finished without the 5 games

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -4,62 +4,60 @@ let ComputerScore = 0;
 
 
 function getComputerChoice(){
-    let list = ["Rock","Paper","Scissors"];
-    let rand = Math.floor(Math.random() * list.length);
-    return list[rand];
+    let list = ["rock","paper","scissors"];
+    return list[(Math.floor(Math.random() * list.length))];
 }
 
 
 function getHumanChoice(){
-    let choice = prompt("Enter Rock Paper or Scissors to play");
-    //console.log(choice,choice.includes("Scissors"));
-    //console.log((choice.localeCompare('rock', undefined, { sensitivity: 'accent' })))
+    let choice = prompt("Enter Rock Paper or Scissors to play").toLowerCase();   
 
     if(((choice.localeCompare('rock', undefined, { sensitivity: 'accent' })) === -1) && ((choice.localeCompare('paper', undefined, { sensitivity: 'accent' })) === -1) && ((choice.localeCompare('scissors', undefined, { sensitivity: 'accent' })) === -1)){
-        //alert("You need to enter Rock Paper or Scissors to play");
-        throw new Error("You need to enter Rock Paper or Scissors to play");  
-        //choice = prompt("Enter Rock Paper or Scissors to play");      
+        throw new Error("You need to enter Rock Paper or Scissors to play");      
     }
     return choice;
 
 }
 
 
-
-
-function playRound(computerChoice, humanChoice){
-    let res ;
+function playRound(humanChoice, computerChoice){
+    let res = "";
     
     switch(true){
-        case(humanChoice === 'rock' && computerChoice === 'scissors'):
+        case(humanChoice == "rock" && computerChoice == "scissors"):
             res = console.log("You won! You chose",humanChoice,"and computer chose", computerChoice);
+            humanScore += 1; 
             break;
-        case(humanChoice === 'scissors' && computerChoice === 'paper'):
+        case(humanChoice === "scissors" && computerChoice === "paper"):
             res = console.log("You won! You chose",humanChoice,"and computer chose", computerChoice);
+            humanScore += 1; 
             break;
-        case(humanChoice === 'rock' && computerChoice === 'paper'):
-            res =console.log("You won! You chose",humanChoice,"and computer chose", computerChoice);
+        case(humanChoice === "rock" && computerChoice === "paper"):
+            res =console.log("You lose! You chose",humanChoice,"and computer chose", computerChoice);
+            ComputerScore += 1;
             break;
-        case(humanChoice === 'paper' && computerChoice === 'scissors'):
-            res = console.log("You won! You chose",humanChoice,"and computer chose", computerChoice);
+        case(humanChoice === "paper" && computerChoice === "scissors"):
+            res = console.log("You lose! You chose",humanChoice,"and computer chose", computerChoice);
+            ComputerScore += 1;
             break;
         default :
-            res = console.log("Draw, you both choose ", humanChoice);
+            res = console.log("Draw");
         }
     
     return res;
+
+    
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice() ;
-
-humanChoice.toLowerCase();
-computerChoice.toLowerCase();
-
-console.log(humanChoice);
-
-playRound();
-//console.log(getHumanChoice());
-//console.log(getComputerChoice());
 
 
+function playGame(){
+    
+    for(let i; i < 6; i++){
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice() ;
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+playGame();
